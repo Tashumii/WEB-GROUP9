0067c4b7496f847abecee1ccbe469138f9cc300d
alvinn00 <cornwallduchy485@gmail.com>
2026-09-19 16:59:47 +0800

    Update database.sql

--- backend/database.sql
+++ backend/database.sql
@@ -1,2 +1,42 @@
--- alvin dito mo lagay lahat ng sql commands na gagamitin mo sa database.
+( User )
+
+CREATE TABLE users (
+    id CHAR(36) PRIMARY KEY DEFAULT (UUID()),
+    role ENUM('Admin', 'Staff') NOT NULL,
+    business_id CHAR(36) NOT NULL
+);
+
+( SALE )
+
+CREATE TABLE sale (
+    id CHAR(36) PRIMARY KEY DEFAULT (UUID()),
+    item_id CHAR(36) NOT NULL,
+    quantity INT NOT NULL,
+    unit_price DECIMAL(10,2) NOT NULL,
+    total DECIMAL(10,2) NOT NULL,
+    sold_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    recorded_by CHAR(36) NOT NULL,
+    FOREIGN KEY (item_id) REFERENCES product(id),
+    FOREIGN KEY (recorded_by) REFERENCES users(id)
+);
+
+( Business )
+
+CREATE TABLE business (
+    id CHAR(36) PRIMARY KEY DEFAULT (UUID()),
+    name VARCHAR(100) NOT NULL
+);
+
+( Products )
+
+CREATE TABLE product (
+    id CHAR(36) PRIMARY KEY DEFAULT (UUID()),
+    name VARCHAR(100) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    stock_qty INT NOT NULL,
+    status ENUM('Active', 'Inactive') NOT NULL DEFAULT 'Active',
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+
 
